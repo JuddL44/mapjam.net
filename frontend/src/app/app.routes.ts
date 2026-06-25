3905c6ba8121508fdceaa5ab2697f366b78f31b8
JuddL44 <businessjudd@gmail.com>
2026-06-25 08:30:22 -0500

    Add login routing page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './routes/home/home';
 import { Mapjams } from './routes/mapjams/mapjams';
 import { Info } from './routes/info/info';
 import { Admin } from './routes/admin/admin';
+import { Login } from './routes/login/login';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'MapJams', component: Mapjams },
   { path: 'Info', component: Info },
   { path: 'Admin', component: Admin },
+  { path: 'Login', component: Login },
 ];
